c55f59e66931b325833f4f07736a5b5efaff9449
SuRu <su33.dev@gmail.com>
2021-01-08 23:48:09 +0200

    cleaned postgres bootstrap scripts

--- docker/scala-bank-db/docker-entrypoint-initdb.d/01_init.sql
+++ docker/scala-bank-db/docker-entrypoint-initdb.d/01_init.sql
@@ -1,7 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE ROLE scalabank PASSWORD 'scalabank_password' LOGIN;
 
 CREATE DATABASE scalabank;
+CREATE DATABASE scalabank_tests;
 
 GRANT ALL PRIVILEGES ON DATABASE scalabank TO scalabank;
+GRANT ALL PRIVILEGES ON DATABASE scalabank_tests TO scalabank;
--- docker/scala-bank-db/docker-entrypoint-initdb.d/02_init_tests.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE DATABASE scalabank_tests;
-
-GRANT ALL PRIVILEGES ON DATABASE scalabank_tests TO scalabank;
